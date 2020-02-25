23464d24cfa3e8e2febc67dcc1a88563d74d7415
fractalswift <matthewthomasmacklin@gmail.com>
2020-02-25 12:31:33 +0000

    finished version 1, getting ready for deployment

--- iati_xml/settings.py
+++ iati_xml/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'iati_xml.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iati',
+        'USER': 'postgres',
+        'PASSWORD': 'koolthing',
+        'HOST': 'localhost',
+        'PORT':'5432',
     }
 }
 
